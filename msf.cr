e83afe98cbc5106e6907a5adbc1e387b387a7a3c
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-09 00:16:57 -0400

    refactored to faciliate adding other MSF algorithms
    I don't like this design, but it's sufficient to proceed with.

--- msf.cr
+++ msf.cr
@@ -1,4 +1,6 @@
-# Finds and visualizes a minimum spanning forest.
+# msf -  Finds and visualizes a minimum spanning forest.
+
+require "bit_array"
 
 # Read-only view into a collection, supporting some array-like operations.
 class ReadOnlyView(C)
@@ -78,14 +80,19 @@ class DisjointSets
   end
 end
 
+# An edge in a weighted undirected graph.
+struct Edge
+  getter u : Int32
+  getter v : Int32
+  getter weight : Int32
+
+  def initialize(@u, @v, @weight)
+  end
+end
+
 # A weighted undirected graph to compute and output a minimum spanning forest.
 class Graph
-  property name = "MSF"
-  property indent = 4
-  property keep_color = "red"
-  property discard_color = "gray"
-
-  @edges = [] of NamedTuple(u: Int32, v: Int32, weight: Int32)
+  @edges = [] of Edge
 
   def initialize(order : Int32)
     @order = order
@@ -94,38 +101,23 @@ class Graph
   def add_edge(u, v, weight)
     raise ArgumentError.new("vertex u out of range") unless 0 <= u < @order
     raise ArgumentError.new("vertex v out of range") unless 0 <= v < @order
-    @edges << {u: u, v: v, weight: weight}
+    @edges << Edge.new(u, v, weight)
   end
 
-  def draw_msf(io = STDOUT)
-    keeps = msf_edge_bits
-
-    margin = " " * @indent
-    io.puts %(graph "#{name}" {)
-
-    # Add the vertices in ascending order, to be drawn as circle.
-    (0...@order).each do |vertex|
-      io.puts %(#{margin}#{vertex} [shape="circle"])
-    end
-    io.puts
-
-    # Add the edges in the order given, colorized according to MSF membership.
-    keeps.each_with_index do |keep, i|
-      edgespec = %(#{@edges[i][:u]} -- #{@edges[i][:v]})
-      colorspec = %(color="#{keep ? keep_color : discard_color}")
-      labelspec = %(label="#{@edges[i][:weight]}")
-      io.puts "#{margin}#{edgespec} [#{colorspec} #{labelspec}]"
-    end
-
-    io.puts "}"
+  def kruskal_msf(io = STDOUT)
+    selection = EdgeSelection.new(@order,
+                                  ReadOnlyView.new(@edges),
+                                  kruskal_msf_edge_bits)
+    selection.name = "MSF (Kruskal)"
+    selection
   end
 
   # Gets an array of bits, where bit i is true iff @edges[i] is in the MSF.
-  private def msf_edge_bits
+  private def kruskal_msf_edge_bits
     sets = DisjointSets.new(@edges.size)
-    keeps = [false] * @edges.size
+    keeps = BitArray.new(@edges.size)
     sorted_edge_indices.each do |i|
-      keeps[i] = sets.union(@edges[i][:u], @edges[i][:v])
+      keeps[i] = sets.union(@edges[i].u, @edges[i].v)
     end
     keeps
   end
@@ -134,14 +126,46 @@ class Graph
   # weight. In case of ties, earlier (i.e. first-given) edges win.
   private def sorted_edge_indices
     (0...@edges.size).to_a.sort! do |i, j|
-      by_weight = @edges[i][:weight] <=> @edges[j][:weight]
+      by_weight = @edges[i].weight <=> @edges[j].weight
       by_weight.zero? ? i <=> j : by_weight
     end
   end
 end
 
 class EdgeSelection
+  property name = "(untilted)"
+  property indent = 4
+  property keep_color = "red"
+  property discard_color = "gray"
 
+  def initialize(@order : Int32,
+                 @edges : ReadOnlyView(Array(Edge)),
+                 @selection : BitArray)
+  end
+
+  # TODO: Implement a comparison function like same_selection? so a single
+  #       run can both emit and compare Kruskal and Prim results.
+
+  def draw(io = STDOUT)
+    margin = " " * @indent
+    io.puts %(graph "#{@name}" {)
+
+    # Emit the vertices in ascending order, to be drawn as circle.
+    (0...@order).each do |vertex|
+      io.puts %(#{margin}#{vertex} [shape="circle"])
+    end
+    io.puts
+
+    # Emit the edges in the order given, colorized according to selection.
+    @selection.each_with_index do |selected, i|
+      edgespec = %(#{@edges[i].u} -- #{@edges[i].v})
+      colorspec = %(color="#{selected ? keep_color : discard_color}")
+      labelspec = %(label="#{@edges[i].weight}")
+      io.puts "#{margin}#{edgespec} [#{colorspec} #{labelspec}]"
+    end
+
+    io.puts "}"
+  end
 end
 
 # Read an order and list of weighted edges as a graph.
@@ -156,4 +180,4 @@ def read_graph(io)
   graph
 end
 
-read_graph(ARGF).draw_msf
+read_graph(ARGF).kruskal_msf.draw
